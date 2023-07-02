6ac155ac3e0d04c60a8b57ac2fba9c0730314c2e
jakub-bednarek <jakub.bednarek1009@gmail.com>
2023-07-02 21:23:24 +0200

    Proper error codes, function syntax cleanup

--- src/data_structures/include/data_structures/hash_map.h
+++ src/data_structures/include/data_structures/hash_map.h
@@ -5,18 +5,17 @@
 #include <stdint.h>
 
 #define MEMORY_ALLOCATION_FAILED 1
-#define NULL_DATA_STRUCT 2
-#define HASH_MAP_FULL 3
-#define KEY_ALREADY_IN_MAP 4
-#define KEY_NOT_IN_MAP 5
+#define HASH_MAP_IS_NULL 2
+#define KEY_IN_MAP 3
+#define KEY_NOT_IN_MAP 4
 
 typedef void* any_val_t;
+typedef void* map_t;
 
-struct hash_map_t;
-
-extern struct hash_map_t* hash_map_create(size_t size);
-extern int32_t hash_map_add(struct hash_map_t* map, const char* key, any_val_t val);
-extern any_val_t hash_map_get(struct hash_map_t* map, const char* key);
-extern int32_t hash_map_remove(struct hash_map_t* hash_map, const char* key);
+extern map_t hash_map_create(size_t size);
+extern int32_t hash_map_add(map_t map, const char* key, any_val_t data);
+extern int32_t hash_map_get(map_t map, const char* key, any_val_t* data);
+extern int32_t hash_map_remove(map_t hash_map, const char* key);
+extern int32_t hash_map_free(map_t map);
 
 #endif // HASH_MAP_C
