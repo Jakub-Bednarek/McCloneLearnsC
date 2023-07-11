3e9d39f677d8851850b628f578999dd11eaa7677
jakub-bednarek <jakub.bednarek1009@gmail.com>
2023-07-12 00:28:24 +0200

    UT runner finished

--- cmake/unit_test.cmake
+++ cmake/unit_test.cmake
@@ -1,5 +1,6 @@
 set(UNITY_DIR ${CMAKE_SOURCE_DIR}/vendor/Unity)
 set(UNITY_LIB Unity)
+set(DEFAULT_UT_BIN_DIR ${CMAKE_SOURCE_DIR}/build/ut/bin)
 
 if(UT_ENABLED)
     message(STATUS "Unit tests config enabled")
@@ -7,6 +8,13 @@ if(UT_ENABLED)
 
     add_library(${UNITY_LIB} STATIC ${UNITY_DIR}/src/unity.c)
     target_include_directories(${UNITY_LIB} PUBLIC ${UNITY_DIR}/src)
+
+    if("${UT_BIN_DIR}" STREQUAL "")
+        message(WARNING "Unit tests bin directory not set. Defaulting to ${DEFAULT_UT_BIN_DIR}")
+        set(UT_BIN_DIR ${DEFAULT_UT_BIN_DIR})
+    else()
+        message(STATUS "Unit Tests dir: ${UT_BIN_DIR}")
+    endif()
 endif()
 
 function(add_ut_subdirectory DIR)
@@ -27,6 +35,7 @@ function(add_ut)
     set(multiValueArgs HEADERS SOURCES LIBRARIES)
     cmake_parse_arguments(UT_ARGS "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    add_executable(${UT_ARGS_TARGET} ${UT_ARGS_HEADERS} ${UT_ARGS_SOURCES})
-    target_link_libraries(${UT_ARGS_TARGET} ${UT_ARGS_LIBRARIES} ${UNITY_LIB})
+    add_executable(test_${UT_ARGS_TARGET} ${UT_ARGS_HEADERS} ${UT_ARGS_SOURCES})
+    target_link_libraries(test_${UT_ARGS_TARGET} ${UT_ARGS_LIBRARIES} ${UNITY_LIB})
+    set_target_properties(test_${UT_ARGS_TARGET} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${UT_BIN_DIR})
 endfunction()
